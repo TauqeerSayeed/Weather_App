3df96ccf70fdd9e8710b93e343113eec0809c879
Tauqeer Sayeed <20btcs042hy@manuu.edu.in>
2024-02-18 01:19:42 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./App.css";
+import SearchForm from "./SearchForm";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="weather-app-wrapper">
+          <div className="weather-app rounded-4">
+            <SearchForm defaultCity="Hyderabad" />
+          </div>
+          <footer>
+            Open-source code by tauqeer_sayeed on {""}
+            <a
+              href="https://github.com/anndcodes/react-weather-app"
+              target="_blank"
+            >
+              <strong>Github</strong>
+            </a>
+          </footer>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
